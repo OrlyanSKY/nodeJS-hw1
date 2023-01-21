fd3e4c2f922d28957ac8a3de483be7c23bde57be
orlyanSKY <000slim001@gmail.com>
2023-01-21 18:41:04 +0300

    fist task has done

--- index.js
+++ index.js
@@ -5,10 +5,43 @@ const {
   addContact,
 } = require("./contacts");
 
-// listContacts().then((contacts) => console.table(contacts));
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// getContactById("5").then((contact) => console.table(contact));
+program.parse(process.argv);
 
-// removeContact(8).then((contacts) => console.table(contacts));
+const argv = program.opts();
 
-addContact("Marik", "Marik@gmail.com", "0930335642");
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.table(contact);
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
